52c0648b1d7989778e189d34fe64f5279fe81bfd
Yeung Wang Sang <240021235@stu.vtc.edu.hk>
2025-04-01 22:06:36 +0800

    testing

--- script/script-general.js
+++ script/script-general.js
@@ -1,20 +1,20 @@
 const headerTemplate_general = `
     <div class="logo">
-        <a href="/index.html"><img src="/img/logo/logo.PNG" alt="logo"></a>
+        <a href="./index.html"><img src="./img/logo/logo.PNG" alt="logo"></a>
     </div>
     <div class="headline">
         <h2>Smile & Sunshine Toy Co, Ltd.</h2>
         <h3>We are the best toy manufacturing company in the World !</h3>
     </div>
     <div class="btn">
-        <a href="/login.html" class="btn">Login | Register</a>
-        <a href="/report-problem.html" class="btn">Report a problem</a>
+        <a href="./login.html" class="btn">Login | Register</a>
+        <a href="./report-problem.html" class="btn">Report a problem</a>
     </div>
     `;
 
 const headerTemplate_login = `
     <div class="logo">
-        <a href="/index.html"><img src="/img/logo/logo.PNG" alt="logo"></a>
+        <a href="./index.html"><img src="./img/logo/logo.PNG" alt="logo"></a>
     </div>
     <div class="headline">
         <h2>Smile & Sunshine Toy Co, Ltd.</h2>
@@ -29,17 +29,17 @@ const headerTemplate_login = `
 const navTemplate_general = `
     <ul>
         <div class="div-nav-margin"/>
-        <li><a href="/about-us.html">About Us</a></li>
+        <li><a href="./about-us.html">About Us</a></li>
     </ul>
     `;
 
 const navTemplate_staff = `
     <ul>
         <div class="div-nav-margin"/>
-        <li><a href="/staff/manage-products.html">Manage Products</a></li>
-        <li><a href="/staff/manage-materials.html">Manage Materials</a></li>
-        <li><a href="/staff/manage-orders.html">Manage Orders</a></li>
-        <li><a href="/staff/generate-report.html">Generate Report</a></li>
+        <li><a href="./staff/manage-products.html">Manage Products</a></li>
+        <li><a href="./staff/manage-materials.html">Manage Materials</a></li>
+        <li><a href="./staff/manage-orders.html">Manage Orders</a></li>
+        <li><a href="./staff/generate-report.html">Generate Report</a></li>
     </ul>
     <div id="user-info-box">
         <p>Welcome, User</p>
@@ -50,10 +50,10 @@ const navTemplate_staff = `
 const navTemplate_customer = `
     <ul>
         <div class="div-nav-margin"/>
-        <li><a href="/customer/browse-products.html">Browse Products</a></li>
-        <li><a href="/customer/place-orders.html">Order Cart</a></li>
-        <li><a href="/customer/manage-orders.html">Manage Orders</a></li>
-        <li><a href="/customer/update-profile.html">Update Profile</a></li>
+        <li><a href="./customer/browse-products.html">Browse Products</a></li>
+        <li><a href="./customer/place-orders.html">Order Cart</a></li>
+        <li><a href="./customer/manage-orders.html">Manage Orders</a></li>
+        <li><a href="./customer/update-profile.html">Update Profile</a></li>
     </ul>
     <div id="user-info-box">
         <p>Welcome, User</p>
@@ -77,12 +77,12 @@ function Login(event) {
     event.preventDefault();
     userType = document.querySelector('input[name="userType"]:checked').value;
     localStorage.setItem("role", userType);
-    window.location.href = "/index.html";
+    window.location.href = "./index.html";
 }
 
 function Logout() {
     localStorage.removeItem("role");
-    window.location.href = "/login.html";
+    window.location.href = "./login.html";
 }
 
 currentRole = localStorage.getItem("role");
