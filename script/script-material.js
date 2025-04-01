fbca52de9256b1f36f8f7f72b553123a102d40ad
Yeung Wang Sang <240021235@stu.vtc.edu.hk>
2025-04-01 17:37:33 +0800

    upload file

--- script/script-material.js
+++ script/script-material.js
@@ -1,2 +1,24 @@
 // get the material data 
 
+//vaild form 
+function validateForm() {
+    const form = document.forms[0];
+    const fileInput = document.getElementById('product-image');
+    const maxFileSize = 25 * 1024 * 1024;
+
+    for (let i = 0; i < form.length; i++) {
+        if (form[i].hasAttribute('required') && !form[i].value) {
+            alert('Please fill out all required fields.');
+            form[i].focus();
+            return false;
+        }
+        
+    if (fileInput.files.length > 0 && fileInput.files[0].size > maxFileSize) {
+        alert('The file size must be less than 25 MB.');
+        fileInput.focus();
+        return false;
+    }
+
+    return true;
+}
+}
--- script/script-product.js
+++ script/script-product.js
@@ -1,3 +1,29 @@
 //get the MySQL data from the database
 
 //get the material check box value form the Mysql database 
+
+//check the edit-product form is valid or not
+function validateForm() {
+    const quantityInput = document.getElementById('product-quantity');
+    const checkboxes = document.querySelectorAll('input[name="Material ID"]');
+    let isChecked = false;
+
+    if (quantityInput.value < 0) {
+        alert("Quantity cannot be less than 0.");
+        return false;
+    }
+
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            isChecked = true;
+        }
+    });
+
+    if (!isChecked) {
+        alert("Please select at least one material.");
+        return false;
+    }
+
+    alert("Form submitted successfully!");
+    return true;
+}
